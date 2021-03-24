0db92d4fe868f6a463e4e0ccf24a89f08a5bc096
Vladislav S <69107440+vladislavs28@users.noreply.github.com>
2021-03-25 00:07:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ig-clone
+ Instagram clone using Laravel
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" class="rounded-circle" style="max-width: 150px;">
+        </div>
+        <div class="col-9 pt-5">
+            <h1>Name</h1>
+            <div class="d-flex">
+                <div class="pr-4"><strong>121</strong> posts</div>
+                <div class="pr-4"><strong>211</strong> followers</div>
+                <div class="pr-4"><strong>1231</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">namename</div>
+            <div>Description Description Description Description Description </div>
+            <div><a href="#">website.com</a></div>
+        </div>
+    </div>
+</div>
+@endsection
